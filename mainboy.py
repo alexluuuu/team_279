60aa278ced73a2f0b1ac2362af303b9392be687a
winst13 <winstonwang12@gmail.com>
2017-12-02 06:05:19 -0800

    segmentation added for realsies lel

--- mainboy.py
+++ mainboy.py
@@ -1,14 +1,28 @@
 from utils.lbp import *
 from utils.DatasetPrep import PrepareGroundTruth
+from utils.segmentation import *
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
 
-	ground_truth_csv_path = "sample_dataset/ISIC-2017_Training_Part3_GroundTruth.csv"
-	
-	print "it's me, the main boy"
+    ground_truth_csv_path = "sample_dataset/ISIC-2017_Training_Part3_GroundTruth.csv"
+    
+    print "it's me, the main boy"
 
-	scale_adap_boy = SALBP()
-	input_names = ['sample_dataset/ISIC_0000000.jpg']
-	scale_adap_boy.ComputeLBP(input_names[0])
-	print "done"
-	#scale_adap_boy.VisualizeLBP()
+    scale_adap_boy = SALBP()
+    input_names = ['sample_dataset/ISIC_0000000.jpg']
+    scale_adap_boy.ComputeLBP(input_names[0])
+    print "done"
+    #scale_adap_boy.VisualizeLBP()
+    
+    #yung main boy does some segmentation
+    img = plt.imread('sample_dataset/ISIC_0000000.jpg')
+    segment = compute_segmentation(img, 2, clustering_fn=kmeans_fast, feature_fn=color_features, scale=0.1)
+    
+    plt.subplot(1, 2, 1)
+    plt.imshow(img)
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(segment, cmap='viridis')
+
+    plt.show()
